4aa195449349c0b82c12babc1a9f9b43a6669f0a
Olle Tengnér <olle.code@hotmail.com>
2024-05-16 19:21:17 +0200

    Add project files.

---
+++ Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppLexi
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+}
---
+++ TodoDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppLexi;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
